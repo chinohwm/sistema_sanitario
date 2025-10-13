966d13412725d75247722b4906dfbe5c239d6e5a
Sebas <cardozosebastian106@gmail.com>
2025-10-13 12:17:58 -0300

    Terminar auh

---
+++ admin/AUH.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AUH</title>
+</head>
+<body>
+    <H1>Datos de auh</H1>
+    <a href="/sistema_sanitario/admin/cargar/cargar_auh.php">
+        <button>Cargar</button>
+    </a>
+</body>
+</html>
---
+++ admin/cargar/cargar_auh.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auh Cargar</title>
+</head>
+<body>
+    <h1>Cargar datos de auh</h1>
+</body>
+</html>
